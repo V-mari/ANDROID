717dc64d27da8b15b74cc07de1de3229b0979ac9
V-mari <82954569+V-mari@users.noreply.github.com>
2021-12-08 09:32:42 -0400

    examen de 2da instancia Mariela Ruth Trujillo

---
+++ app/src/main/java/com/m/android/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.m.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+    }
+
+    public void goCreateAccount(View view) {
+    }
+}
